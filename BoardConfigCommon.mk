cab5962c3692ef88264ae6e56ef9cf326b0f820f
Ethan Chen <intervigil@gmail.com>
2013-06-06 20:34:53 -0700

    s4-common: add custom recovery graphics
    Change-Id: I0a4047fe42ee0146b0daf270d37c271f1d86e832

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -62,6 +62,7 @@ TARGET_PROVIDES_LIBLIGHT := true
 TARGET_PROVIDES_POWERHAL := true
 
 # Recovery
+BOARD_CUSTOM_GRAPHICS := ../../../device/htc/s4-common/recovery/graphics.c
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 

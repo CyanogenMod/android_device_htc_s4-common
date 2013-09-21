ebdf1331bead2ba661c1332dd79eb25ab0517c37
Ethan Chen <intervigil@gmail.com>
2013-09-20 18:11:25 -0700

    s4-common: fix copy paste
    Change-Id: I9b4951c3a162af50d5b9209984c620ed2a343eb1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,7 +57,7 @@ COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA_HARDWARE
 BOARD_HAVE_NEW_QC_GPS := true
 
 # Graphics
-TARGET_DISPLAY_INSECURE_MM_HEAP : true
+TARGET_DISPLAY_INSECURE_MM_HEAP := true
 
 # Recovery
 BOARD_CUSTOM_GRAPHICS := ../../../device/htc/s4-common/recovery/graphics.c

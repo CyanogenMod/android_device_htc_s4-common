46382b8aa2ce71f9dbf7930331c6559fc4151797
jared caliendo <kushdeck@gmail.com>
2013-12-24 02:00:06 -0800

    s4-common: Build prima_wlan into kernel.
    * Remove module defines as we no longer build prima_wlan as a module
    * Increase ramdisk_offset to allow to support larger boot.img
    
    Change-Id: Ic7679cfaf74db449fbb7d023ba6492e683787e27

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,7 +37,7 @@ TARGET_SPECIFIC_HEADER_PATH := device/htc/s4-common/include
 BOARD_KERNEL_BASE := 0x80400000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # Audio
@@ -87,8 +87,6 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/prima_wlan/parameters/fwpath"
-WIFI_DRIVER_MODULE_NAME := prima_wlan
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/prima_wlan.ko"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version

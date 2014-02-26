f8b1107d2f0f043c6baa5c8038592b4696a854d1
Matt Mower <mowerm@gmail.com>
2014-02-26 09:50:02 -0600

    s4-common: move WCNSS_qcom_cfg.ini to device tree
    Seems reasonable to me that configuration changes go through gerrit and
    there's nothing really proprietary about this file.
    
    Change-Id: I16cd68f32fd68aead4177cce54e0fbfb9361e551

--- s4.mk
+++ s4.mk
@@ -69,7 +69,8 @@ PRODUCT_COPY_FILES += \
 # Wifi config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:/system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:/system/etc/wifi/p2p_supplicant_overlay.conf
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:/system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:/system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini
 
 # Permissions
 PRODUCT_COPY_FILES += \

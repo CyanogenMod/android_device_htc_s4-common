80d0f37385e1028d4e74bd14ddd95feaefc8d910
Ethan Chen <intervigil@gmail.com>
2014-06-10 19:30:29 -0700

    s4-common: Use wcnss_service to initialize wifi
    Change-Id: Ia98a0048f3c7b7fee29d2df34b5fa1b128169ecb

--- s4.mk
+++ s4.mk
@@ -65,7 +65,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:/system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:/system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:/system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:/system/etc/wifi/WCNSS_qcom_cfg.ini
+
+PRODUCT_PACKAGES += \
+    libwcnss_qmi \
+    wcnss_service
 
 # Permissions
 PRODUCT_COPY_FILES += \

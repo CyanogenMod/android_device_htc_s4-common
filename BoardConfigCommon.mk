e2dc58fd595005d3ef2214302d71f9b78ff6acfc
Matt Mower <mowerm@gmail.com>
2014-06-28 03:19:12 -0500

    s4-common: remap capacitive keys in CWM
    Back = Back (unchanged)
    Home = Down
    Recent = Select
    
    Change-Id: I708b352eeed87e826dd7af0958356e09e1283b44

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,6 +73,7 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 BOARD_RECOVERY_SWIPE := true
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/htc/s4-common/recovery/source/recovery_keys.c
 
 # Charge mode
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode

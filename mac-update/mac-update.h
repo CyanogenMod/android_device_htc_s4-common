4f3a8c1d4619525dd58bfba8ffbc21d31c73e1d8
Matt Mower <mowerm@gmail.com>
2014-08-16 15:48:06 -0500

    s4-common: modify mac-update for HTC and utilize
    This updates the WCNSS NV bin file with the correct MAC address.
    
    Change-Id: Ie92c31610f19528b8fac090024b810ef467d19a8

--- mac-update/mac-update.h
+++ mac-update/mac-update.h
@@ -1,12 +1,24 @@
-/* To FreeXperia from a friend :) */
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2014 FreeXperia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #define NV_IN "/system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin"
 #define NV_OUT "/data/misc/wifi/prima/WCNSS_qcom_wlan_nv.bin"
 
-#define MAC0_FILE "/data/etc/wlan_macaddr0"
-#define MAC1_FILE "/data/etc/wlan_macaddr1"
-#define MAC2_FILE "/data/etc/wlan_macaddr2"
-#define MAC3_FILE "/data/etc/wlan_macaddr3"
+#define CALIBRATION_PATH "/proc/calibration"
 
 #define MAC0_OFFSET 10
 #define MAC1_OFFSET 16

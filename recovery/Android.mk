18e707b784f477eb03bd5e51ad24379cd7972e74
Matt Mower <mowerm@gmail.com>
2014-09-14 15:06:49 -0500

    s4-common: use offmode charging blobs from m7
    * Use offmode charging blobs from m7 (m7wlv OTA 5.28.605.2) to enable
      charging images support
    * Shrink charging images from m7:
      - fireball/ville: 50% reduction (1080px --> 540px screen width)
      - evita/jewel: 66.67% reduction (1080px --> 720px screen width)
    
    Change-Id: Ida40ad57ceb0fb5669f2adb840f31c7b0e242cca

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -25,11 +25,11 @@ LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= power_test
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= EXECUTABLES
-LOCAL_SRC_FILES		:= sbin/power_test
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
+LOCAL_MODULE        := power_test
+LOCAL_MODULE_TAGS   := optional eng
+LOCAL_MODULE_CLASS  := EXECUTABLES
+LOCAL_SRC_FILES     := sbin/power_test
+LOCAL_MODULE_PATH   := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -41,10 +41,67 @@ LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= detect_key
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= EXECUTABLES
-LOCAL_SRC_FILES		:= sbin/detect_key
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
+LOCAL_MODULE        := detect_key
+LOCAL_MODULE_TAGS   := optional eng
+LOCAL_MODULE_CLASS  := EXECUTABLES
+LOCAL_SRC_FILES     := sbin/detect_key
+LOCAL_MODULE_PATH   := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_PREBUILT)
 
+define _add-offmode_charging-image
+include $$(CLEAR_VARS)
+LOCAL_MODULE := offmode_charging_$(notdir $(1))
+LOCAL_MODULE_STEM := $(notdir $(1))
+_img_modules += $$(LOCAL_MODULE)
+LOCAL_SRC_FILES := $1
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $$(TARGET_RECOVERY_ROOT_OUT)/res/offmode_charging_images
+include $$(BUILD_PREBUILT)
+endef
+
+_img_modules :=
+ifneq ($(filter fireball ville,$(TARGET_DEVICE)),)
+$(foreach _img, $(call find-subdir-subdir-files, "res/offmode_charging_images-540", "*.png"), \
+  $(eval $(call _add-offmode_charging-image,$(_img))))
+else
+$(foreach _img, $(call find-subdir-subdir-files, "res/offmode_charging_images-720", "*.png"), \
+  $(eval $(call _add-offmode_charging-image,$(_img))))
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := offmode_charging_res_images
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := $(_img_modules)
+include $(BUILD_PHONY_PACKAGE)
+
+define _add-offmode_charging_warn-image
+include $$(CLEAR_VARS)
+LOCAL_MODULE := offmode_charging_warn_$(notdir $(1))
+LOCAL_MODULE_STEM := $(notdir $(1))
+_img_modules += $$(LOCAL_MODULE)
+LOCAL_SRC_FILES := $1
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $$(TARGET_RECOVERY_ROOT_OUT)/res/offmode_charging_warn_images
+include $$(BUILD_PREBUILT)
+endef
+
+_img_modules :=
+ifneq ($(filter fireball ville,$(TARGET_DEVICE)),)
+$(foreach _img, $(call find-subdir-subdir-files, "res/offmode_charging_warn_images-540", "*.png"), \
+  $(eval $(call _add-offmode_charging_warn-image,$(_img))))
+else
+$(foreach _img, $(call find-subdir-subdir-files, "res/offmode_charging_warn_images-720", "*.png"), \
+  $(eval $(call _add-offmode_charging_warn-image,$(_img))))
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := offmode_charging_warn_res_images
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := $(_img_modules)
+include $(BUILD_PHONY_PACKAGE)
+
+_add-offmode_charging-image :=
+_add-offmode_charging_warn-image :=
+_img_modules :=
--- s4.mk
+++ s4.mk
@@ -44,11 +44,11 @@ COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     lpm.rc \
-    choice_fn \
-    power_test \
-    offmode_charging \
     chargeled \
-    detect_key
+    choice_fn \
+    offmode_charging \
+    offmode_charging_res_images \
+    offmode_charging_warn_res_images
 
 # Audio config
 PRODUCT_COPY_FILES += \

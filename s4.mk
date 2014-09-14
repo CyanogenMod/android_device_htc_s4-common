42762648f2ec3f23d98c9e14f290900e4ff29a5d
Matt Mower <mowerm@gmail.com>
2014-09-14 15:06:49 -0500

    s4-common: use offmode charging blobs from m7
    * Use offmode charging blobs from m7 (m7wlv OTA 5.28.605.2) to enable
      charging images support
    * Shrink charging images from m7:
      - fireball/ville: 50% reduction (1080px --> 540px screen width)
      - evita/jewel: 66.67% reduction (1080px --> 720px screen width)
    
    Change-Id: Ida40ad57ceb0fb5669f2adb840f31c7b0e242cca

--- s4.mk
+++ s4.mk
@@ -44,11 +44,13 @@ COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     lpm.rc \
-    choice_fn \
-    power_test \
-    offmode_charging \
     chargeled \
-    detect_key
+    choice_fn \
+    detect_key \
+    offmode_charging \
+    offmode_charging_res_images \
+    offmode_charging_warn_res_images \
+    power_test
 
 # Audio config
 PRODUCT_COPY_FILES += \

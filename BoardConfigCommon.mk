LineageOS/android_device_samsung_msm8930-common

sepolicy: Address netd denials
Change-Id: I72314672a56c138d48c48912de96cb5daddc14c8

BoardConfigCommon.mk
@@ -92,6 +92,7 @@ BOARD_SEPOLICY_UNION += \
     mediaserver.te \
     mm-qcamerad.te \
     mpdecision.te \
+    netd.te \
     property_contexts \
     property.te \
     qseecomd.te \

CyanogenMod/android_device_htc_s4-common

BoardConfigCommon.mk
@@ -86,7 +86,8 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_UNION += \
     akmd.te \
     ewtzmud.te \
-    kernel.te
+    kernel.te \
+    netd.te
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
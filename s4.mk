125f4a392f2e6b5676a2bf261afb75032d08c9a8
Dan Pasanen <invisiblek@cyanogenmod.org>
2015-12-01 08:09:00 -0600

    s4-common: Shim htc_logging lib
    Change-Id: Icf943c7dee874fe437943c795d30642c118221ea

--- s4.mk
+++ s4.mk
@@ -36,16 +36,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     camera.msm8960
 
-# Ramdisk
-PRODUCT_PACKAGES += \
-    init.qcom.power.rc \
-    init.qcom.usb.rc \
-    init.qcom.rc \
-    ueventd.qcom.rc
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/init.qcom.bt.sh:/system/etc/init.qcom.bt.sh
-
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8960
@@ -53,8 +43,22 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.qcom.power.rc \
+    init.qcom.usb.rc \
+    init.qcom.rc \
+    ueventd.qcom.rc
+
 # Recovery
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+
+# Shims
+PRODUCT_PACKAGES += \
+    libshim_log
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init.qcom.bt.sh:/system/etc/init.qcom.bt.sh
 
 # Stlport
 PRODUCT_PACKAGES += \

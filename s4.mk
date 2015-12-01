f129531706ad1a89687859ef8e6ea5975aa16700
Dan Pasanen <invisiblek@cyanogenmod.org>
2015-12-01 08:09:00 -0600

    s4-common: Shim htc_logging lib
    Change-Id: Icf943c7dee874fe437943c795d30642c118221ea

--- s4.mk
+++ s4.mk
@@ -43,6 +43,10 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     ueventd.qcom.rc
 
+# Shims
+PRODUCT_PACKAGES += \
+    libshim_log
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/init.qcom.bt.sh:/system/etc/init.qcom.bt.sh
 

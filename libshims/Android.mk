07f2b00776bdf08bb3bfd8715e8ff14203431753
Christopher R. Palmer <crpalmer@gmail.com>
2016-02-14 07:50:29 -0500

    sensors: Properly shim VectorImpl
    VectorImpl needs to have virtual methods added to the object itself.
    You cannot do this by simply adding the symbols, the object itself
    needs to be created with the correct sized virtual function table.
    
    Create a separate lib that does this shimming because we need to
    add to each shared library which makes use of the VectorImpl symbols
    to make sure the libcutils versions aren't used.
    
    Change-Id: I3677c55b819fe891f9d577fd6e26447c45c46555

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -35,10 +35,13 @@ LOCAL_SRC_FILES := \
      ui/GraphicBufferAllocator.cpp \
      ui/GraphicBufferMapper.cpp \
      MemoryHeapPmem.cpp \
-     MemoryBase.c
+     MemoryBase.c \
+     SharedBuffer.cpp \
+     VectorImpl.cpp
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync libshim_sensors
 LOCAL_MODULE := libshim_camera
+LOCAL_C_INCLUDES += external/safe-iop/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_MODULE_TAGS := optional
@@ -49,8 +52,11 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
     sensors.c \
-    VectorImpl.c
+    SharedBuffer.cpp \
+    VectorImpl.cpp
 
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_C_INCLUDES  += external/safe-iop/include
 LOCAL_MODULE      := libshim_sensors
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)

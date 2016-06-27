f5bd5a05f2cf0a2479519ca5a986f7ec4118908e
Matt Mower <mowerm@gmail.com>
2016-06-27 13:06:39 -0500

    s4-common: Add buildable libpn544_fw
    The libpn544_fw.so blobs from s4 devices have text relocations.
    This library is easily re-generated by extracting the contents of
    nxp_nfc_fw[] and nxp_nfc_full_version[] from libpn544_fw.so on
    jewel 4.13.651.4 (most up-to-date nfc libs relative to evita and
    fireball) and building as a new shared lib.
    
    Change-Id: Iea4a2c5250f7442a1e73314f160acd46ee96ddf1

--- nfc/Android.mk
+++ nfc/Android.mk
@@ -16,6 +16,17 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES := libpn544_fw.c
+LOCAL_MODULE := libpn544_fw
+LOCAL_MODULE_OWNER := nxp
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACK_MODULE_RELOCATIONS := false
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := nfc_hw.c
 
 LOCAL_MODULE := nfc.$(TARGET_BOARD_PLATFORM)

b5300eb53e84f8c97ebec296459a50f831fb1243
Matt Mower <mowerm@gmail.com>
2016-07-21 08:59:35 -0500

    s4-common: Remove NO_SECURE_DISCARD flag
    * Kernel now specifies high capacity erase on sdc1
    
    Change-Id: I8d990db29e4ce4bcf0f7d50b09ceead863633c03

--- s4.mk
+++ s4.mk
@@ -50,9 +50,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
 
-# Recovery
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-
 # Shims
 PRODUCT_PACKAGES += \
     libshim_camera \
